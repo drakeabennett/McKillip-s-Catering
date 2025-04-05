02e61a54332a10bd986af997a796779dfaafa937
drakeabennett <drakeabennett@gmail.com>
2025-04-05 13:57:48 -0700

    Home Page Content Added

--- app.js
+++ app.js
@@ -4,5 +4,17 @@ const menuLinks = document.querySelector('.navbar_menu');
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
-
 });
+
+window.addEventListener("resize", function () {
+    if (window.innerWidth > 960) {
+        document.body.classList.remove("menu-active");
+        menu.classList.remove('is-active');
+        menuLinks.classList.remove('active');
+    }
+});
+
+document.getElementById("mobile-menu").addEventListener("click", function () {
+    document.body.classList.toggle("menu-active");
+});
+
